af432cebe85620c086a6babb2639385462c309d4
Solon97 <solonb20@gmail.com>
2026-07-15 17:13:31 -0300

    chore(infra): scaffold CDK app

---
+++ infra/lib/config.ts
@@ -0,0 +1,19 @@
+export interface ServiceConfig {
+  serviceName: 'order-service';
+  containerPort: number;
+  publicPath: '/orders';
+  healthCheckPath: '/health';
+  cpu: number;
+  memoryLimitMiB: number;
+  desiredCount: number;
+}
+
+export const serviceConfig: ServiceConfig = {
+  serviceName: 'order-service',
+  containerPort: 3000,
+  publicPath: '/orders',
+  healthCheckPath: '/health',
+  cpu: 512,
+  memoryLimitMiB: 1024,
+  desiredCount: 1,
+};
